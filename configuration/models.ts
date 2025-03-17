682cb482af478fe4cf4ff35ebd9246782ee3a14a
aad171 <97544224+aad171@users.noreply.github.com>
2025-03-17 18:10:13 -0400

    Update models.ts
    changed all to fine tuned

--- configuration/models.ts
+++ configuration/models.ts
@@ -1,14 +1,14 @@
 import { ProviderName } from "@/types";
 
-export const INTENTION_MODEL: string = "gpt-4o-mini";
+export const INTENTION_MODEL: string = "ft:gpt-4o-2024-08-06:default-project:run-5:B8ALN2db";
 export const INTENTION_TEMPERATURE: number = 0.7;
 
 export const RANDOM_RESPONSE_PROVIDER: ProviderName = "openai";
-export const RANDOM_RESPONSE_MODEL: string = "gpt-4o-mini";
+export const RANDOM_RESPONSE_MODEL: string = "ft:gpt-4o-2024-08-06:default-project:run-5:B8ALN2db";
 export const RANDOM_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const HOSTILE_RESPONSE_PROVIDER: ProviderName = "openai";
-export const HOSTILE_RESPONSE_MODEL: string = "gpt-4o-mini";
+export const HOSTILE_RESPONSE_MODEL: string = "ft:gpt-4o-2024-08-06:default-project:run-5:B8ALN2db";
 export const HOSTILE_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const QUESTION_RESPONSE_PROVIDER: ProviderName = "openai";
