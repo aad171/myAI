05d7e43597d34b6d820ce808a44c24dc449aa045
aad171 <97544224+aad171@users.noreply.github.com>
2025-03-17 17:05:20 -0400

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,8 +3,8 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `CreditCompanion`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Aadit M @ UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `Your Credit Guru`;
+export const CLEAR_BUTTON_TEXT: string = `New Conversation`;
+export const PAGE_TITLE: string = `CreditCompanion`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
