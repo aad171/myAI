e3f1c074d7eea7ab6a4bf6ba5761b6ca8a6be3eb
aad171 <97544224+aad171@users.noreply.github.com>
2025-03-17 18:42:50 -0400

    Update identity.ts
    Added tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Aadit Mehta`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `CreditCompanion`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Educational, Helpful, Guiding, Informative, Supportive, Encouraging`;
 export const AI_ROLE: string = `[AI ROLE]`;
