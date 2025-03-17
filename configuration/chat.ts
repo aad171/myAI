719b99f3cea7edb366eb41a2aa41b7b1b1759acc
aad171 <97544224+aad171@users.noreply.github.com>
2025-03-17 16:53:32 -0400

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}. My goal is to help you and other young people navigate the complexity the credit card world. Try asking me questions like "How do credit cards work" or even as specific as "I am 22 years old, about to graduate college and start working making $100k in Chapel Hill, NC, what are some credit cards that might be good for me to look at applying for considering I will primarily be spending on travel, groceries, rent, and gas?" `;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
