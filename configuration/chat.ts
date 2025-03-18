0999ec4d79632b8619ca1219383cecad8c1435a8
aad171 <97544224+aad171@users.noreply.github.com>
2025-03-17 23:19:27 -0400

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,12 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}. My goal is to help you and other young people navigate the complexity the credit card world. Try asking me questions like "How do credit cards work" or even as specific as "I am 22 years old, about to graduate college and start working making $100k in Chapel Hill, NC, what are some credit cards that might be good for me to look at applying for considering I will primarily be spending on travel, groceries, rent, and gas?" `;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}.
+
+My goal is to help you and other young people navigate the complexities of the credit card world. You can ask me questions like "How do credit cards work?" or "Why is credit important?" and I'll do my best to explain these to you.
+
+We can also address more specific inquiries, such as: "I am 22 years old, about to graduate college and start working with a salary of $100k in Chapel Hill, NC. What are some credit cards that might be good for me to consider, given that I will primarily be spending on travel, groceries, rent, and gas?"
+
+Whatever questions you may have related to credit and credit cards, I'm here to help!`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
